9750cd790d3549f83d3c4924199ea737db53e1ca
Grishenkov <hitori-a@yandex.ru>
2026-03-10 20:29:36 +0500

    init project

---
+++ src/main/java/com/example/msaccountreservation2/MsAccountReservation2Application.java
@@ -0,0 +1,13 @@
+package com.example.msaccountreservation2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountReservation2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAccountReservation2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msaccountreservation2/MsAccountReservation2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaccountreservation2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountReservation2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
